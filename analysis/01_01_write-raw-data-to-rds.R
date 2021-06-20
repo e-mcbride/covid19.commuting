e171bc16265d20a0c31f1827fdffa98883df6cc1
Elizabeth McBride <mcbrideelizabethc@gmail.com>
2021-06-20 08:36:14 -0700

    Added expanded survey data to write rds script

--- analysis/01_01_write-raw-data-to-rds.R
+++ analysis/01_01_write-raw-data-to-rds.R
@@ -5,12 +5,19 @@ library(here)
 
 # melt_csv imports data where each cell is given its own row with columns of metadata
 readr::melt_csv(here("analysis/data/raw_data/raw-covid19-survey.csv")) |>
-
   # write file as .rds for faster import in future
   readr::write_rds(here("analysis/data/derived_data/raw-data-cells.rds"))
 
 
 readr::read_csv(here("analysis/data/raw_data/raw-covid19-survey.csv")) |>
   readr::write_rds(here("analysis/data/derived_data/raw-data.rds"))
+
+
+
+readr::read_csv(here("analysis/data/raw_data/raw-covid19-survey-expanded.csv")) |>
+  readr::write_rds(here("analysis/data/derived_data/raw-data-expanded.rds"))
+
+melt_csv(here("analysis/data/raw_data/raw-covid19-survey-expanded.csv")) |>
+  readr::write_rds(here("analysis/data/derived_data/raw-data-expanded-cells.rds"))
 
 
