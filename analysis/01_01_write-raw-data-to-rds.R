8b1884b387bbf5bd08046bd69afb995846c3be54
Elizabeth McBride <mcbrideelizabethc@gmail.com>
2021-06-20 19:31:12 -0700

    Added potential function to write data to .rds

--- analysis/01_01_write-raw-data-to-rds.R
+++ analysis/01_01_write-raw-data-to-rds.R
@@ -3,6 +3,11 @@
 here::i_am("analysis/01_01_write-raw-data-to-rds.R")
 library(here)
 
+csv_to_rds <- function(csvin, rdsout) {
+  readr::read_csv(csvin) |>
+    readr::write_rds(rdsout)
+}
+
 # melt_csv imports data where each cell is given its own row with columns of metadata
 readr::melt_csv(here("analysis/data/raw_data/raw-covid19-survey.csv")) |>
   # write file as .rds for faster import in future
@@ -14,10 +19,10 @@ readr::read_csv(here("analysis/data/raw_data/raw-covid19-survey.csv")) |>
 
 
 
-readr::read_csv(here("analysis/data/raw_data/raw-covid19-survey-expanded.csv")) |>
+readr::read_csv(here("analysis/data/raw_data/raw-covid19-survey-expanded.csv"), col_names = FALSE) |>
   readr::write_rds(here("analysis/data/derived_data/raw-data-expanded.rds"))
 
-melt_csv(here("analysis/data/raw_data/raw-covid19-survey-expanded.csv")) |>
+readr::melt_csv(here("analysis/data/raw_data/raw-covid19-survey-expanded.csv")) |>
   readr::write_rds(here("analysis/data/derived_data/raw-data-expanded-cells.rds"))
 
 
