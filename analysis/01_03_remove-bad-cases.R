28fc109f7eb7229e6cb62ffbc844b31f808bbc39
Elizabeth McBride <mcbrideelizabethc@gmail.com>
2021-06-22 20:15:59 -0700

    First commit: script to remove bad cases from data

---
+++ analysis/01_03_remove-bad-cases.R
@@ -0,0 +1,12 @@
+### Remove cases that are incomplete or unusable for whatever reason
+
+here::i_am("analysis/01_03_remove-bad-cases.R")
+library(here)
+library(tidyverse)
+# Compare the respondent IDs in the "complete" vs the "incomplete included"
+
+# Find out why the "incompletes" were removed OR just remove them and move on for speed
+
+
+
+
