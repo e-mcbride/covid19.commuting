2151040a7d316feecc5f34bf06b51e3fb4a5878d
Elizabeth McBride <mcbrideelizabethc@gmail.com>
2021-07-01 20:09:25 -0700

    Fixed naming conventions for Mplus

--- analysis/temp_rm-cases-brute-force.R
+++ analysis/temp_rm-cases-brute-force.R
@@ -10,9 +10,32 @@ library(tidyverse)
 dictionary <- read_rds(here("analysis/data/derived_data/clean-question-list.rds"))
 data_newnames <- read_rds(here("analysis/data/derived_data/data-newnames-completeonly.rds"))
 
-data_short <- data_newnames |>
-  select(-collectorid, -startdate, -enddate, -custom_data_1, -collector_type_source, -device, -contains("_mode_"),
-         -region_survey_monk, -united_states_region_survey_monk)
+data_short <- data_newnames %>%
+  # select(-collectorid, -startdate, -enddate, -custom_data_1, -collector_type_source, -device, -contains("_mode_"),
+  #        -region_survey_monk, -united_states_region_survey_monk)
+  select(-(collid:coll_type_source), -(device:length(.)), -contains("mod_"))
+  # select(contains("time"), contains("dst"), contains("ntr"))
+
+
+
+# Get travel variable names -----------------------------------------------
+
+trav_prefix <- c("time", "dst", "ntr") %>% str_c(collapse = "|")
+trav_regx <- paste0("^(b4)([a-z0-9_]+)", "(", trav_prefix, ")")
+
+
+# string_cnames <- data_short %>% colnames()
+
+
+trav_varnames <- data_short %>%
+  # extract column names as vector
+  colnames() %>%
+  str_subset(trav_regx)
+
+trav_varnames
+
+mode_regx <- str_extract(trav_varnames, "([a-z0-9]+)$") %>% unique() %>% str_c(collapse = "|")
+
 
 
 
@@ -54,47 +77,38 @@ data_short %>%
 
 # vars_to_change <- c("time_", "dist_", "trips")
 
-trav_prefix <- c("time_", "dist_", "trips") %>% str_c(collapse = "|")
-trav_regx <- paste0("^(be4_)([a-z0-9_]+)", "(", trav_prefix, ")")
+paste0("(", trav_prefix,")", "_([a-z0-9_]+)")
 
-
-string_cnames <- data_elim %>% colnames()
-
-
-trav_varnames <- data_elim %>%
-  # extract column names as vector
-  colnames() %>%
-  str_subset(trav_regx)
-
-mode_regx <- str_extract(trav_varnames, "([a-z0-9]+)$") %>% unique() %>% str_c(collapse = "|")
-# mode_regx <- paste0("(", trav_mode, ")")
-
-
+# mode totals, ignoring work or school or everything else
 mode_totals <- data_elim %>%
   select(pid, contains(trav_varnames)) %>%
   gather(key = "colname", value = "value", -pid) %>%
   mutate(
     # grp = str_extract(colname, "time|dist|trips"),
-    mode = str_extract(colname, "(time|dist|trips)_([a-z0-9_]+)")) %>%
+    mode = str_extract(colname, paste0("(", trav_prefix,")", "_([a-z0-9_]+)"))) %>%
   group_by(pid, mode) %>%
-  summarise(be4_tot = sum(value)) %>%
-  spread(key = mode, value = be4_tot)
+  summarise(b4_tot = sum(value)) %>%
+  spread(key = mode, value = b4_tot)
 
 
 # drive alone, bike/walk/transit, carpool
+eco <- "bik|wlk|trn" #eco-friendly: bike, walk, transit
+dal <- "dal" # drive alone
+shr <- "dot|pass" # sharing/carpooling
+
 abbr_mode_totals <- data_elim %>%
   select(pid, contains(trav_varnames)) %>%
   gather(key = "colname", value = "value", -pid) %>%
   mutate(
-    measure = str_extract(colname, "time|dist|trips"),
-    travmode = str_extract(colname, paste0("(", trav_mode, ")")),
+    measure = str_extract(colname, trav_prefix),
+    travmode = str_extract(colname, paste0("(", mode_regx, ")")),
     # travmode = str_extract(colname, "([a-z0-9]+)$"),
     # measmode = str_extract(colname, "(time|dist|trips)_([a-z0-9_]+)"),
     abbrmode = case_when(
-      str_detect(travmode, "bike|walk|transit") ~ "eco",
-      str_detect(travmode, "dralone")           ~ "dral",
-      str_detect(travmode, "drothers|pass")     ~ "shar",
-      TRUE                                      ~ "oth"
+      str_detect(travmode, eco) ~ "eco",
+      str_detect(travmode, dal) ~ "dal",
+      str_detect(travmode, shr) ~ "shr",
+      TRUE                      ~ "oth"
     ),
     abbr_meas_mode = str_c(measure, abbrmode, sep = "_")
     ) %>%
@@ -113,13 +127,21 @@ abbr_mode_totals <- data_elim %>%
 data_b4_analyze <- data_elim %>%
   select(pid,
          # SES/demographics
-         gender, be4_employ, hh_inc,
+         gender, b4_emp, hinc,
 
          # attitude
-         starts_with("prompt")
+         starts_with("q")
          ) %>%
-  left_join(abbr_mode_totals, by = "pid") %>%
-  rename_with(~ str_replace(.x, "prompt_", "q"), starts_with("prompt"))
+  left_join(abbr_mode_totals, by = "pid")
+  # rename_with(~ str_replace(.x, "prompt_", "q"), starts_with("prompt"))
+
+#  Mplus only allows 8 char, so any colnames > 8 char?
+((data_b4_analyze %>% colnames() %>% nchar()) > 8) %>% any()
+
+# print column names to copy/paste into Mplus VARIABLE argument
+noquote(colnames(data_b4_analyze))
+an_cn <- colnames(data_b4_analyze)
+str_c(an_cn, collapse = " ") %>% noquote()
 
 
 write_csv(data_b4_analyze, here("analysis/data/derived_data/data-analysis.csv"))
