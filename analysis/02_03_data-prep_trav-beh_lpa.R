fe0b68d57e6d040bd17751cd71ba63817dc94aa8
Elizabeth McBride <mcbrideelizabethc@gmail.com>
2021-07-16 15:49:52 -0700

    First commit: script to prep data for travel behavior LPA in Mplus

---
+++ analysis/02_03_data-prep_trav-beh_lpa.R
@@ -0,0 +1,13 @@
+# 02_03 Data prep: mplus travel behavior LPA
+
+library(tidyverse)
+
+here::here("analysis/02_03_data-prep_trav-beh_lpa.R")
+library(here)
+
+data_b4_analyze <- read_rds(here("analysis/data/derived_data/data-mplus-ready.rds"))
+
+
+
+# save file to `analysis` folder bc that means mplus script can find it with no PATH necessary
+write_csv(data_b4_analyze, na = "-9999", col_names = FALSE, here("analysis/03_Mplus/b4-data-analysis_att-cont.csv"))
