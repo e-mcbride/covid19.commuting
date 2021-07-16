344759a27482e688eaf6f1bc419783fb24546ff7
Elizabeth McBride <mcbrideelizabethc@gmail.com>
2021-07-16 15:29:27 -0700

    Rename 02_02 to more accurately represent script purpose, removed dummy variable code

--- analysis/02_02_data-prep_lpa_attitudes.R
+++ analysis/02_02_data-prep_lpa_attitudes.R
@@ -27,19 +27,19 @@ data_likert_cont <- data_elim %>%
   ) %>%
   # left_join(abbr_mode_totals, by = "pid") %>%
 
-  # Dummy Variables (so far, everything "character" is a dummy variable)
+  # Identify factor variables
   mutate(across(where(is.character), forcats::as_factor)) %>%
-  # mutate(hinc = fct_relevel(hinc, c("$0",
-  #                                   "Under $9,999",
-  #                                   "$10,000 to $24,999",
-  #                                   "$25,000 to $34,999",
-  #                                   "$35,000 to $49,999",
-  #                                   "$50,000 to $74,999",
-  #                                   "$75,000 to $99,999",
-  #                                   "$100,000 to $149,999",
-  #                                   "$150,000 to $199,999",
-  #                                   "$200,000 to $249,999",
-  #                                   "$250,000 or more"))) %>%
+  mutate(hinc = fct_relevel(hinc, c("$0",
+                                    "Under $9,999",
+                                    "$10,000 to $24,999",
+                                    "$25,000 to $34,999",
+                                    "$35,000 to $49,999",
+                                    "$50,000 to $74,999",
+                                    "$75,000 to $99,999",
+                                    "$100,000 to $149,999",
+                                    "$150,000 to $199,999",
+                                    "$200,000 to $249,999",
+                                    "$250,000 or more"))) %>%
   mutate(across(starts_with("q_"), fct_relevel, c("Strongly Disagree",
                                                   "Disagree",
                                                   "Neither Agree nor Disagree",
@@ -50,10 +50,11 @@ data_likert_cont <- data_elim %>%
   mutate(across(starts_with("score_"), ordered)) %>%
   rename_with(~gsub("score_", "", .x, fixed = TRUE))
 
+# print column names to copy/paste into Mplus syntax
 an_coln <- colnames(data_likert_cont) %>% str_c(collapse = " ") %>% noquote()
 an_coln
 
-write_file(an_coln, here("analysis/data/derived_data/analysis-colnames.txt"))
+# write_file(an_coln, here("analysis/data/derived_data/analysis-colnames.txt"))
 
 
 write_csv(data_likert_cont, na = "-9999", col_names = FALSE, here("analysis/03_Mplus/data-likert-cont.csv"))
