cffd70a325bfbbfb0adb2cc019541b83708895f7
Elizabeth McBride <mcbrideelizabethc@gmail.com>
2021-07-16 15:16:25 -0700

    cleanup of 02_01

--- analysis/02_01_vars-for-analysis.R
+++ analysis/02_01_vars-for-analysis.R
@@ -33,7 +33,6 @@ mode_totals <- data_elim %>%
   select(pid, contains(trav_varnames)) %>%
   gather(key = "colname", value = "value", -pid) %>%
   mutate(
-    # grp = str_extract(colname, "time|dist|trips"),
     mode = str_extract(colname, paste0("(", trav_prefix,")", "_([a-z0-9_]+)"))) %>%
   group_by(pid, mode) %>%
   summarise(b4_tot = sum(value)) %>%
@@ -93,7 +92,7 @@ data_b4_analyze <- data_elim %>%
   ) %>%
   left_join(abbr_mode_totals, by = "pid") %>%
 
-  # Dummy Variables (so far, everything "character" is a dummy variable)
+  # Identify factor variables
   mutate(across(where(is.character), forcats::as_factor)) %>%
   mutate(hinc = fct_relevel(hinc, c("$0",
                                     "Under $9,999",
@@ -134,24 +133,8 @@ data_b4_analyze %>%
   dplyr::select(where(is.factor)) %>%
   map(levels)
 
-# ATTEMPT =====
-
-
-#
-# dumdum <- data_b4_analyze %>%
-#   mutate(across(where(is.factor), as.numeric, .names = "score_{.col}")) %>%
-#   select(-(where(is.factor))) %>%
-#   mutate(across(starts_with("score_"), ordered)) %>%
-#   recipe(dst_dal ~ .) %>%
-#   step_dummy(starts_with("score_"), one_hot = TRUE) %>%
-#   prep() %>%
-#   bake(x) %>%
-#   rename_with(~gsub("score_", "", .x, fixed = TRUE))
-
-
-# dumbs %>% rename_with(~gsub("score_", "", .x, fixed = TRUE)) %>% View()
-
 #  Mplus only allows 8 char, so any colnames > 8 char?
+
 # ((data_dum %>% colnames() %>% nchar()) > 8) %>% which()
 ((data_b4_analyze %>% colnames() %>% nchar()) > 8) %>% which()
 
@@ -170,7 +153,7 @@ an_coln
 
 write_file(an_coln, here("analysis/data/derived_data/analysis-colnames.txt"))
 
-# save file to `analysis` folder bc that means mplus script can find it with no PATH necessary9
+# save file to `analysis` folder bc that means mplus script can find it with no PATH necessary
 # write_csv(data_dum, na = "-9999", col_names = FALSE, here("analysis/03_Mplus/b4-data-analysis.csv"))
 write_csv(data_b4_analyze, na = "-9999", col_names = FALSE, here("analysis/03_Mplus/b4-data-analysis_att-cont.csv"))
 
