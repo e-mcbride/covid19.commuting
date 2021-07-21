99a1f4b8d691ed076eabf3ee764dc6fc879ae5aa
Elizabeth McBride <mcbrideelizabethc@gmail.com>
2021-07-20 17:08:07 -0700

    Analysis running scripts commented out to keep them from overwriting results

--- analysis/04_02_run_LPA_att.R
+++ analysis/04_02_run_LPA_att.R
@@ -1,7 +1,13 @@
 # 04_02 create model syntax
-
-createModels(templatefile = here("analysis/03_Mplus/attitudes/lpa_attitudes_template.txt"))
-
-runModels(
-  here("analysis/03_Mplus/attitudes/"),
-  recursive=TRUE)
+# library(tidyverse)
+# library(MplusAutomation)
+# library(here)
+#
+# #import my function to write mplus data to file in the right file location
+# devtools::load_all()
+#
+# createModels(templatefile = here("analysis/03_Mplus/attitudes/lpa_attitudes_template.txt"))
+#
+# runModels(
+#   here("analysis/03_Mplus/attitudes/"),
+#   recursive=TRUE)
--- analysis/05_02_run_LPA_ttds.R
+++ analysis/05_02_run_LPA_ttds.R
@@ -1,7 +1,13 @@
 # 05_02 create model syntax for travel time and distance LPA
-
-createModels(templatefile = here("analysis/03_Mplus/trav-beh/time_dist/lpa_ttds_template.txt"))
-
-runModels(
-  here("analysis/03_Mplus/attitudes/"),
-  recursive=TRUE)
+# library(tidyverse)
+# library(MplusAutomation)
+# library(here)
+#
+# #import my function to write mplus data to file in the right file location
+# devtools::load_all()
+#
+# createModels(templatefile = here("analysis/03_Mplus/trav-beh/time_dist/lpa_ttds_template.txt"))
+#
+# runModels(
+#   here("analysis/03_Mplus/trav-beh/time_dist//"),
+#   recursive=TRUE)
