c3ef47a689c15f48b68db5365d6882c84261aff4
Elizabeth McBride <mcbrideelizabethc@gmail.com>
2021-07-23 14:58:22 -0700

    Removing ppl who do neither work nor school, rerunning time models

---
+++ analysis/unused-scripts/time-models_WS-only.R
@@ -0,0 +1,87 @@
+# remove the no-work and no-school ppl and run models w/o them
+library(tidyverse)
+library(here)
+library(MplusAutomation)
+library(janitor)
+devtools::load_all()
+
+
+
+# Extract pids of ppl who were not working or in school before pandemic
+data_elim <- read_rds("analysis/data/derived_data/data-good-cases.rds")
+
+pids_no_WS <- data_elim %>%
+  filter(b4_emp == "No" & stu == "No") %>%
+  pull(pid)
+
+
+
+# Build new dataset without them ========
+
+data_mplus_ready <- read_rds(here("analysis/data/derived_data/data-mplus-ready.rds"))
+
+
+# take mplus-ready data, filter out the non-working or schooling ppl
+timeWS_mplus <- data_mplus_ready %>%
+  select(pid, starts_with("time")) %>%
+  filter(!(pid %in% pids_no_WS)) %>%
+  # filter(!(pid %in% smallclasspids)) %>%
+  data.frame()
+
+
+
+write_mplus_data(df = timeWS_mplus,
+                 wd_for_analysis = here("analysis/03_Mplus/trav-beh/timeWS/"),
+                 filename = "timeWS-data-mplus-ready.dat",
+                 writeData = "ifmissing",
+                 hashfilename = TRUE)
+
+# Create travel time B models =============
+
+
+createModels(templatefile = here("analysis/03_Mplus/trav-beh/timeWS/timeWS_lpa-template.txt"))#, recursive = F)
+
+runModels(
+  here("analysis/03_Mplus/trav-beh/timeWS/"),
+  recursive=F)
+
+
+
+# Analyze travel time models, update and re-run ===================
+# Imports =====
+
+allOut_tWS <- readModels(
+  here("analysis/03_Mplus/trav-beh/timeWS/"),
+  recursive = FALSE)
+
+# Get the table of values =====
+ttWS_outs <- allOut_tWS %>%
+  enframe() %>%
+  transmute(name,
+            LLRepTbl = map(value, LLrep_to_table),
+            summaries = map(value, ~ .x$summaries),
+            nclasses = map(summaries, "NLatentClasses"),
+            Loglikelihood = map(summaries, "LL"),
+            BIC = map(summaries, "BIC"),
+            ABIC = map(summaries, "aBIC"),
+            BLRT_pval = map(summaries, "BLRT_PValue"),
+            VLMRT_pval = map(summaries, "T11_VLMR_PValue"),
+            Entropy = map(summaries, "Entropy"),
+            llnreps = map(value, LLreplication),
+            optseed = map(LLRepTbl,
+                          ~ .x %>% slice(1) %>% pull(seed)),
+            seedused = map(value, ~ .x$input$analysis$optseed),
+            t11_km1ll = map(summaries, "T11_KM1LL")
+  )
+
+
+# run after getting some BLRT and VLMRT results
+fitstats <- ttWS_outs %>%
+  select(-name, -LLRepTbl, -summaries, -llnreps, -optseed, -seedused, -t11_km1ll) %>%
+  unnest(cols = c(nclasses, Loglikelihood, BIC, ABIC, BLRT_pval, VLMRT_pval,
+                  Entropy))
+
+ggplot(ttWS_outs, aes(x = as.numeric(nclasses))) +
+  geom_line(aes(y = as.numeric(ABIC), color = "red")) +
+  geom_line(aes(y = as.numeric(BIC), color = "blue")) +
+  scale_color_discrete(name = "Legend", labels = c("ABIC", "BIC"))
