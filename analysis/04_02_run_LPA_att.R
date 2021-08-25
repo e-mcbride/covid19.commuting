93af5b21b7c677be9386d6d2a5462af0e0014d4b
Elizabeth McBride <mcbrideelizabethc@gmail.com>
2021-08-25 10:23:40 -0700

    Note to self

--- analysis/04_02_run_LPA_att.R
+++ analysis/04_02_run_LPA_att.R
@@ -7,7 +7,7 @@ library(here)
 devtools::load_all()
 
 # createModels(templatefile = here("analysis/03_Mplus/attitudes/lpa_attitudes_template.txt"))
-
+## IN THE FUTURE: have the template files built by createModels in a separate folder, then moved to the "run" folder to protect work.
 runModels(
   here("analysis/03_Mplus/attitudes/"),
   recursive=TRUE)
