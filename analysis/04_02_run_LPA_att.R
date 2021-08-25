e6651109bc4d64ce913a99c09ab868ef7fcb81ed
Elizabeth McBride <mcbrideelizabethc@gmail.com>
2021-08-25 10:22:26 -0700

    commented out createModels so it doesn't mess up my work.

--- analysis/04_02_run_LPA_att.R
+++ analysis/04_02_run_LPA_att.R
@@ -6,7 +6,7 @@ library(here)
 #import my function to write mplus data to file in the right file location
 devtools::load_all()
 
-createModels(templatefile = here("analysis/03_Mplus/attitudes/lpa_attitudes_template.txt"))
+# createModels(templatefile = here("analysis/03_Mplus/attitudes/lpa_attitudes_template.txt"))
 
 runModels(
   here("analysis/03_Mplus/attitudes/"),
